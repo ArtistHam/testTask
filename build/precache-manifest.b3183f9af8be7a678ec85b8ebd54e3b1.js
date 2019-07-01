ff8f01b7bd69c8aa5ae0fce4f51be534aab05ad0
Vasiliy Kashapov <great.artist.ham@gmail.com>
2019-07-01 13:20:11 +0300

    build

---
+++ build/precache-manifest.b3183f9af8be7a678ec85b8ebd54e3b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5edd56d128ed6a9a8575df2ef7406af3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "90ee09afb6415c1127f0",
+    "url": "/static/css/main.fd3f3d2a.chunk.css"
+  },
+  {
+    "revision": "95299049074ab30ffeb0",
+    "url": "/static/js/2.68ea1001.chunk.js"
+  },
+  {
+    "revision": "90ee09afb6415c1127f0",
+    "url": "/static/js/main.c3accb4f.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  }
+]);
